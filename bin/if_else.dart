79d8bbf72a0eb8dfd363ba6b068972a48934d97b
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-07 12:37:09 +0600

    first commit

---
+++ bin/if_else.dart
@@ -0,0 +1,12 @@
+void main() {
+  int x = 30;
+  int y = 30;
+
+  if (x > y) {
+    print("x greater than y");
+  } else if (x < y) {
+    print("y greater than x");
+  } else {
+    print("x equal to y");
+  }
+}
